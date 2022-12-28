8ff3ed1b6471ebbc262d34a051f2d08c5207bbe8
jhangkar <taqit3852@gmail.com>
2022-12-28 16:47:11 +0600

    task update done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 
 require('dotenv').config()
@@ -18,7 +18,7 @@ app.use(express.json());
 // password 2mOGNlk8dlDooPXF
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 })
 
 
@@ -28,42 +28,68 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
-  if(err){
+  if (err) {
     console.log(err);
   }
-  else{
+  else {
     console.log("database connection successful");
   }
- 
+
 });
 
 
-const run = async()=>{
-    try{
-        const usersCollection = client.db("absoluteTask").collection("users");
-        const tasksCollection = client.db("absoluteTask").collection("tasks");
+const run = async () => {
+  try {
+    const usersCollection = client.db("absoluteTask").collection("users");
+    const tasksCollection = client.db("absoluteTask").collection("tasks");
 
-        app.post('/users', async(req, res)=>{
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
-            res.send(result)
-        })
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
 
-        app.post('/tasks', async(req, res)=>{
-            const task = req.body;  
-            const result = await tasksCollection.insertOne(task);
-            res.send(result)
-        })
+    app.post('/tasks', async (req, res) => {
+      const task = req.body;
+      const result = await tasksCollection.insertOne(task);
+      res.send(result)
+    })
 
-    }
-    finally{
+    app.get('/mytasks', async (req, res) => {
+      const email = req.query.email;
+      const query = { userEmail: email}
+      const result = await tasksCollection.find(query).toArray()
+      console.log(result);
+      const remaining = result.filter(task => task.status === 'Incomplete')
+      res.send(remaining)
+    })
 
-    }
+    app.put('/mytasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const  status= req.body.status
+      const filter = { _id:ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: status
+        },
+      };
+      const result = await tasksCollection.updateOne(filter, updateDoc, options);
+    })
+
+
+
+
+
+  }
+  finally {
+
+  }
 }
 
-run().catch(err=>{console.log(err)})
+run().catch(err => { console.log(err) })
 
 
 app.listen(port, () => {
-    console.log("server listening on port", port);
+  console.log("server listening on port", port);
 })
